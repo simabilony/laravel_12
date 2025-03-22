c4161ecd6726df0ce67dce6f29a914c0eff99cb2
Sima Bilony <www.simabilony@gmail.com>
2025-03-23 01:36:59 +0300

    Navigation and Reusable Main Layout

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
 //});
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
 //Route::get('/second', function () {
 //    return view('second');
     // More simple syntax:
     Route::view('/second', 'second');
-});
+//});
