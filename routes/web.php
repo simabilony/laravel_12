2beeb3b60003f1b8d4b9799dcb11b29ba83dcc43
Sima Bilony <www.simabilony@gmail.com>
2025-03-22 23:59:58 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+//});
+//Route::get('/second', function () {
+//    return view('second');
+    // More simple syntax:
+    Route::view('/second', 'second');
 });
